190515c9c8a81d213955a52e1fa22a4c401623c0
SulaymonGit <buuusuuu7@gmail.com>
2025-02-14 19:55:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// // const use = prompt("Ismingizni kiriting")
+// // const result= use.length
+// // alert(`${use.toLocaleUpperCase}ismi ${result}harftan iborat`)
+// const oquvchi ={
+//     ism:"Sulaymon",
+//     yosh:12,
+//     coin:1169,
+//     group:1746,
+//     uylangan:false,
+//     bolasi:false,
+
+// }
+// console.log(oquvchi.coin);
+const ism = prompt(`ISMINGIZINI kiriting`)
+const fam = prompt(`familiyangizni kiriting`)
+const yosh= prompt("yosh kiriting")
+const oquvchi ={
+     ism:ism,
+     familiya:fam,
+     yosh:yosh,
+}   
+console.log(oquvchi);
+   
